adafd7559c402fb389710c3b4ac930904baa68a6
AlejandroArapiles <61056751+AlejandroArapiles@users.noreply.github.com>
2020-06-08 20:40:48 +0200

    Update UsuarioController.php
    Corrigiendo errores sintacticos clase UsuarioController (modificarUsuario, modificarPerfil)

--- server-app/src/Controller/Api/UsuarioController.php
+++ server-app/src/Controller/Api/UsuarioController.php
@@ -89,7 +89,7 @@ class UsuarioController extends AuthAbstractController
         if (isset($usuario)) {
             $datos = json_decode($request->getContent());
             $usuario->setNombreusuario($datos->nombreusuario);
-            if (!empty($datos->password) {
+            if (!empty($datos->password)) {
                 $usuario->setPassword(md5($datos->password));
             }
             $usuario->setRol($datos->rol);
@@ -110,7 +110,9 @@ class UsuarioController extends AuthAbstractController
         $usuario = $this->getDoctrine()->getRepository(Usuario::class)->findOneByIdusuario($idUsuario);
         if (isset($usuario)) {
             $datos = json_decode($request->getContent());
-            $usuario->setPassword(md5($datos->password));
+            if (!empty($datos->password)) {
+                $usuario->setPassword(md5($datos->password));
+            }
         
             $this->entityManager->persist($usuario);
             $this->entityManager->flush();
